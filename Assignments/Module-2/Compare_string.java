2f6d5c75e4953dcbb3f785f29f10c0ac60da3ab0
deep764 <120081895+deep764@users.noreply.github.com>
2023-03-28 19:48:01 +0530

    Add files via upload

---
+++ Assignments/Module-2/Compare_string.java
@@ -0,0 +1,13 @@
+package Module_2;
+
+public class Compare_string 
+{
+	public static void main(String[] args) 
+	{
+		String s1="topsint.com";
+		String s2="topsint.com";
+		String s3="Topsint.com";
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s3));
+	}
+}
---
+++ Assignments/Module-2/Contents.java
@@ -0,0 +1,12 @@
+package Module_2;
+
+public class Contents 
+{
+	public static void main(String[] args) 
+	{
+		String s1="Java Exercises";
+		String s2="Java Exercise";
+		System.out.println(s1.endsWith("se"));
+		System.out.println(s2.endsWith("se"));
+	}
+}
